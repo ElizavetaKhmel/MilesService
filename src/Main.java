01ab8c4c4fea7f1d3187dde51b375991661816b3
ElizavetaKhmel <137898540+ElizavetaKhmel@users.noreply.github.com>
2023-07-26 00:05:53 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         BonusMilesService service = new BonusMilesService();
         int price = 10_000;
-        int miles = service.calculate(price); // должно получиться 500
+        int miles = service.calculate(price);
         System.out.println(miles);
 
     }
