a1c78ac31af76426e4cf9bdc27ea0022d776399d
ElizavetaKhmel <137898540+ElizavetaKhmel@users.noreply.github.com>
2023-07-26 00:11:44 +0300

    Update Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        BonusMilesService service = new BonusMilesService();
+     MilesService service = new MilesService();
         int price = 10_000;
         int miles = service.calculate(price);
         System.out.println(miles);
